ad48798ce90ba40ddaa3e7c971f2ce0fa0f548bf
offiong-funmilola <fawolefunmilola2@gmail.com>
2023-02-14 18:09:09 +0100

    clearing inputfields

--- src/Components/Certifications.jsx
+++ src/Components/Certifications.jsx
@@ -3,13 +3,13 @@ import { certifications } from './ProfileData';
 
 function Certifications() {
   return (
-    <section id='certification' className='w-screen h-fit p-10'>
+    <section id='certification' className='w-screen h-fit p-10 lg:p-6'>
         <div className='w-full h-full flex flex-col'>
             <h2 className='self-center text-violet-900 text-4xl mb-10'>CERTIFICATIONS</h2>
             <div className='h-11/12 flex flex-col md:flex-row justify-start items-center'>
                 {certifications.map(certification => (
                     <div className="card w-full h-full md:w-1/2 flex justify-center items-center">
-                        <fiqure className='w-11/12 md:w-full'>
+                        <fiqure className='w-11/12 md:w-full lg:w-11/12'>
                             <a href={certification.link} target='_blank' rel="noreferrer">
                                 <img src={certification.image} alt=''></img>
                             </a>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,22 +5,23 @@ import emailjs from '@emailjs/browser';
 function Contact() {
    
     const form = useRef();
-
+    
     const handleSubmission = (e) => {
         e.preventDefault();
-        emailjs.sendForm('service_vduvvu8', 'template_su7p52d', form.current, 'qqz96FvLWg8V_I91L')
-        .then((result) => "Successful",
-        (error) => "Not Submitted");
+        emailjs.sendForm('service_vduvvu8', 'template_0d5mt16', form.current, 'qqz96FvLWg8V_I91L')
+        .then((result) => alert("Successful"),
+        (error) => alert("Not Submitted"));
+        e.target.reset();
     }
   return (
     <section id='contact' className='w-screen h-screen'>
         <div style={{backgroundImage:`url(${contact})`}} className='relative w-full h-full bg-center bg-cover bg-no-repeat flex items-center justify-center'>
-            <div className='w-5/6 h-11/12 px-3 md:px-0 md:w-11/12 bg-white  absolute flex flex-col items-center py-10'>
-                <h2 className='text-violet-900 text-4xl mb-5'>CONTACT</h2>
+            <div className='w-5/6 h-11/12 px-3 md:px-0 md:w-11/12 bg-white  absolute flex flex-col items-center py-10 xl:h-5/6 xl:py-5'>
+                <h2 className='text-violet-900 text-4xl mb-5 xl:mb-3'>CONTACT</h2>
                 <div className="w-full h-full flex flex-col items-center">
                     <h2 className='font-bold text-3xl'>Let's Talk</h2>
                     <div className='w-11/12 h-full flex flex-col items-center lg:justify-center'>
-                        <form ref={form} onSubmit={handleSubmission} className='w-full h-5/6 lg:h-3/4 lg:w-5/6 flex flex-col justify-center px-16 lg:px-10'>
+                        <form ref={form} onSubmit={handleSubmission} className='w-full h-5/6 lg:h-3/4 lg:w-5/6 flex flex-col justify-center px-16 lg:px-8'>
                             <label htmlFor='Name'className='text-xl font-semibold  text-violet-900'> Name
                                 <input type='text' name='name' placeholder='Your Name' className='block w-full px-3 py-3 text-left mb-3 mt-2 border-b-2'/>
                             </label>
@@ -31,7 +32,7 @@ function Contact() {
                                 <textarea name='message' placeholder='Enter your message' className='block w-full px-3 py-6 text-left mb-3 mt-2 border-b-2'></textarea>
                             </label>
                             <div>
-                                <button className='btn btn-primary mt-5'>
+                                <button  className='btn btn-primary mt-5'>
                                     Submit
                                 </button>
                             </div>
